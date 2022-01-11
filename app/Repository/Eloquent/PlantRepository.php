c8fbe442b973b2acc71edaf9f80d7f82d1eefef3
Ivan P <ivan@simplex.agency>
2022-01-11 21:36:12 +0800

    Create end point for publishing plants

--- app/Repository/Eloquent/PlantRepository.php
+++ app/Repository/Eloquent/PlantRepository.php
@@ -2,8 +2,11 @@
 
 namespace App\Repository\Eloquent;
 
+use App\Models\Image;
 use App\Models\Plant;
 use App\Repository\Contracts\Plant\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PlantRepository extends BaseRepository implements Factory
 {
@@ -13,5 +16,44 @@ class PlantRepository extends BaseRepository implements Factory
     public function __construct(Plant $model)
     {
         parent::__construct($model);
+    }
+
+    /**
+     * Upload multiple plant images
+     *
+     * @param Plant $plant
+     * @param Request $request
+     * @return void
+     */
+    public function uploadImages(Plant $plant, Request $request): void
+    {
+        if (!$request->hasFile('files')) {
+            return;
+        }
+
+        $allowedfileExtension=['gif','jpg','png'];
+
+        $files = $request->allFiles();
+
+        foreach ($files as $file) {
+            $extension = $file->getClientOriginalExtension();
+
+            $check = in_array($extension, $allowedfileExtension);
+
+            if (!$check) {
+                continue;
+            }
+
+            $name = $file->getClientOriginalName();
+            $type = 'image/' . $extension;
+            $path = $file->storeAs('public/plants/photos', $name, 'local');
+
+            $plant->images()->save(
+                new Image([
+                    'type' => $type,
+                    'path' => str_replace('public', 'storage', $path),
+                ])
+            );
+        }
     }
 }
