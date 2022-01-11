33b5f7b0ebdd159d48e8274fde8ce9630e6ef6c9
Ivan P <ivan@simplex.agency>
2022-01-11 22:33:26 +0800

    Create Feature Testing for all End Points
    - HTTP test for plants listing end point
    - HTTP test for adding of plants end point

--- tests/Feature/PlantsEndPointTest.php
+++ tests/Feature/PlantsEndPointTest.php
@@ -2,20 +2,53 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class PlantsEndPointTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Check if the plants listing endpoint ([GET] /api/v1/plants) returns correct data
      *
      * @return void
      */
-    public function test_example()
+    public function test_list_all_plants()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/v1/plants');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertJsonStructure()
+            ->assertJson(function (AssertableJson $json) {
+                $json->has('data');
+            });
+    }
+
+    /**
+     * Test adding new a new plant
+     *
+     * @return void
+     */
+    public function test_add_new_plant()
+    {
+        Storage::fake('local');
+
+        $files = [
+            UploadedFile::fake()->image('plant-picture.jpg'),
+        ];
+
+        $response = $this->post('/api/v1/plants', [
+            'name' => 'Blue Oak',
+            'species' => 'Quercus douglasii',
+            'watering_instructions' => 'Lorem ipsum dolor set amet.',
+            'files' => $files,
+        ]);
+
+        foreach ($files as $file) {
+            Storage::disk('local')->assertExists('/public/plants/photos/' . $file->getClientOriginalName());
+        }
+
+        $response->assertStatus(201);
     }
 }
